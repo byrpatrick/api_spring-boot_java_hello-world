c7b8dc85fe5a50f88ec2c06ab3cc76bc605502ca
Dan <dan.arias@auth0.com>
2021-04-27 15:28:16 -0400

    Add CORS support

--- src/main/java/com/example/helloworld/message/MessageController.java
+++ src/main/java/com/example/helloworld/message/MessageController.java
@@ -1,9 +1,11 @@
 package com.example.helloworld.message;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4040")
 @RestController
 @RequestMapping("api/messages")
 public class MessageController {
