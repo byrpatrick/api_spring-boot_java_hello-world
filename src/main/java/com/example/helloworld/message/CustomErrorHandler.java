7684664bba326e3013acd4a9eb952fbdda8eda95
Maciej Glowka <maciejglowka.pl@gmail.com>
2021-05-29 21:57:32 +0200

    returning custom responses for 400s and 500s http statuses

---
+++ src/main/java/com/example/helloworld/message/CustomErrorHandler.java
@@ -0,0 +1,46 @@
+package com.example.helloworld.message;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@ControllerAdvice
+public class CustomErrorHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    Map<String, String> handleInternalServerError(Exception e) {
+        Map<String, String> response = new HashMap<>();
+
+        String errorStatus = "Internal server error occurred" + getExceptionMessage(e);
+        response.put("status", errorStatus);
+
+        return response;
+    }
+
+
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        HashMap<String, String> response = new HashMap<>();
+        response.put("message", "no handler available for given url");
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+
+    }
+
+    private String getExceptionMessage(Exception e) {
+        return Objects.isNull(e.getMessage()) ? "" : " cause: " + e.getMessage();
+    }
+}
--- src/main/java/com/example/helloworld/security/SecurityConfig.java
+++ src/main/java/com/example/helloworld/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.helloworld.security;
 
+import com.example.helloworld.message.Message;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -15,64 +17,80 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.util.List;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-  @Value("${auth0.audience}")
-  private String audience;
+    @Value("${auth0.audience}")
+    private String audience;
 
-  @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-  private String issuer;
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuer;
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests()
-        .mvcMatchers(HttpMethod.GET, "/api/messages/public").permitAll()
-        .anyRequest()
-        .authenticated()
-        .and()
-        .cors()
-        .configurationSource(corsConfigurationSource())
-        .and()
-        .oauth2ResourceServer()
-        .jwt()
-        .decoder(jwtDecoder())
-        .jwtAuthenticationConverter(jwtAuthenticationConverter());
-  }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers(HttpMethod.GET, "/api/messages/public").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .cors()
+                .configurationSource(corsConfigurationSource())
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint((request, response, authException) -> {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    try (PrintWriter writer = response.getWriter()) {
+                        writer.print(new ObjectMapper().writeValueAsString(new Message("trying to access secured endpoint with an unauthenticated user")));
+                    }
+                })
+                .accessDeniedHandler((request, response, accessDeniedException) -> {
+                    try (PrintWriter writer = response.getWriter()) {
+                        writer.print(new ObjectMapper().writeValueAsString(new Message("access denied")));
+                    }
+                })
+                .and()
+                .oauth2ResourceServer()
+                .jwt()
+                .decoder(jwtDecoder())
+                .jwtAuthenticationConverter(jwtAuthenticationConverter())
+        ;
+    }
 
-  CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedMethods(List.of(
-        HttpMethod.GET.name(),
-        HttpMethod.PUT.name(),
-        HttpMethod.POST.name(),
-        HttpMethod.DELETE.name()
-    ));
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedMethods(List.of(
+                HttpMethod.GET.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.DELETE.name()
+        ));
 
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration.applyPermitDefaultValues());
-    return source;
-  }
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration.applyPermitDefaultValues());
+        return source;
+    }
 
-  JwtDecoder jwtDecoder() {
-    OAuth2TokenValidator<Jwt> withAudience = new AudienceValidator(audience);
-    OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
-    OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withAudience, withIssuer);
+    JwtDecoder jwtDecoder() {
+        OAuth2TokenValidator<Jwt> withAudience = new AudienceValidator(audience);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+        OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withAudience, withIssuer);
 
-    NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(issuer);
-    jwtDecoder.setJwtValidator(validator);
-    return jwtDecoder;
-  }
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(issuer);
+        jwtDecoder.setJwtValidator(validator);
+        return jwtDecoder;
+    }
 
-  JwtAuthenticationConverter jwtAuthenticationConverter() {
-    JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
-    converter.setAuthoritiesClaimName("permissions");
-    converter.setAuthorityPrefix("");
+    JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
+        converter.setAuthoritiesClaimName("permissions");
+        converter.setAuthorityPrefix("");
 
-    JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
-    jwtConverter.setJwtGrantedAuthoritiesConverter(converter);
-    return jwtConverter;
-  }
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(converter);
+        return jwtConverter;
+    }
 }
